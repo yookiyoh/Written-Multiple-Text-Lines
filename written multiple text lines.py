fa738d8e030e428d557c21aa5ab5d63692175020
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-27 15:05:02 +0800

    begin

--- written multiple text lines.py
+++
@@ -1,4 +0,0 @@
-# Codilan, Ralph Lorenz I.
-# BSCpE 1-5
-# Object-Oriented Programming | Assignment #4 - Problem 3
-# This program writes a method in Python to write multiple lines of text contents into a text file.
