7d21b24848b7c85c3cd59350b5f2277de4d65eff
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-27 17:29:17 +0800

    use of def function for main program execution, opening mylife.txt in write mode

--- written multiple text lines.py
+++ written multiple text lines.py
@@ -3,7 +3,10 @@
 # Object-Oriented Programming | Assignment 4 - Problem 3
 # This program write a method in Python to write multiple lines of text contents into a text file.
 
+def main():
 # open the mylife.txt in write mode
+with open("mylife.txt", "w") as input_file:
+
 
 # use looping to keep asking the user for input
 
