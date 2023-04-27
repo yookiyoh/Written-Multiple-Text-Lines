f280c41c39595b674adff4f41bf67f47fd0d6cf1
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-27 17:26:25 +0800

    pseudocode

---
+++ written multiple text lines.py
@@ -0,0 +1,22 @@
+# Codilan, Ralph Lorenz I.
+# BSCpE 1-5
+# Object-Oriented Programming | Assignment 4 - Problem 3
+# This program write a method in Python to write multiple lines of text contents into a text file.
+
+# open the mylife.txt in write mode
+
+# use looping to keep asking the user for input
+
+# ask the user for input
+
+# write the input to the file
+
+# ask the user if they want to add more lines
+
+# write the input to mylife.txt
+
+# if user input is yes
+  # continue
+
+# if user input is no
+  # exit
